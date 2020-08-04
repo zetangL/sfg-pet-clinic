8924487f7e6732d8f90c2db5708f1d24868662e8
zetangL <zetanglei@gmail.com>
2020-08-04 18:03:51 -0400

    Created multiple Modules. Added in Maven Release Plugin. May still have problems with both

--- pet-clinic-data/src/main/java/springframeworkexample/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/springframeworkexample/sfgpetclinic/model/Vet.java
@@ -1,5 +1,5 @@
 package springframeworkexample.sfgpetclinic.model;
 
 public class Vet extends Person{
-    
+
 }
--- pet-clinic-web/src/test/java/springframeworkexample/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springframeworkexample/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,7 +1,9 @@
 package springframeworkexample.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+//import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
