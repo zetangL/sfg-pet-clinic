44c7c9b28b4d1d736dd3ed4b0b133e63ff27f0b0
zetangL <zetanglei@gmail.com>
2020-08-05 13:41:00 -0400

    Changed to multi Modules Maven project.

--- pet-clinic-web/src/test/java/springframeworkexample/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springframeworkexample/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,9 +1,7 @@
 package springframeworkexample.sfgpetclinic;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-//import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
