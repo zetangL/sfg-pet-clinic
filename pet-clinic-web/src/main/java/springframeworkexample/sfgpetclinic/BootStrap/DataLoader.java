18cf382873bacf0f5436e2c50104e271fc19432f
zetangL <zetanglei@gmail.com>
2020-08-08 18:11:56 -0400

    Adding Vets List and update controller

--- pet-clinic-web/src/main/java/springframeworkexample/sfgpetclinic/BootStrap/DataLoader.java
+++ pet-clinic-web/src/main/java/springframeworkexample/sfgpetclinic/BootStrap/DataLoader.java
@@ -27,9 +27,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Glenanne");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
 
         ownerService.save(owner2);
 
@@ -43,9 +43,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Jessie");
-        vet1.setLastName("Porter");
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
 
         vetService.save(vet2);
 
