3d4da7306ffc7a154a3ee230a7256739dfe36e4b
zetangL <zetanglei@gmail.com>
2020-08-11 16:32:28 -0400

    Added Speciality and added it to vets

--- pet-clinic-data/src/main/java/springframeworkexample/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/springframeworkexample/sfgpetclinic/model/Vet.java
@@ -1,5 +1,15 @@
 package springframeworkexample.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
 
+public class Vet extends Person{
+    private Set<Speciality> speciality;
+
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
